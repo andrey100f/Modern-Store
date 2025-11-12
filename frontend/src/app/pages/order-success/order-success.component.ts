2c7d7e9eaadf8a7cb5ded37d6157f92461f5a249
andrey100f <andreiforminte19@yahoo.com>
2025-11-12 20:18:09 +0200

    Added UI for order success component

--- frontend/src/app/pages/order-success/order-success.component.ts
+++ frontend/src/app/pages/order-success/order-success.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import {MatIcon} from '@angular/material/icon';
+import {MatButton} from '@angular/material/button';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-order-success',
-  imports: [],
+  imports: [
+    MatIcon,
+    MatButton,
+    RouterLink
+  ],
   templateUrl: './order-success.component.html',
   styleUrl: './order-success.component.scss',
 })
