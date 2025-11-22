89ff98aa164b23cedfdd9cb9bc9e30b76110b9c6
andrey100f <andreiforminte19@yahoo.com>
2025-11-22 11:01:47 +0200

    Add wishlist module with initial configuration and application setup

---
+++ backend/wishlist/src/test/java/com/ubb/modernstore/wishlist/WishlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ubb.modernstore.wishlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishlistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
