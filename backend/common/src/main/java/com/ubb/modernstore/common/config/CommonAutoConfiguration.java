9081c1d542f64838efc199f13d3bcfb14f781630
andrey100f <andreiforminte19@yahoo.com>
2026-01-23 11:21:59 +0200

    Removed old backend folder

--- backend/common/src/main/java/com/ubb/modernstore/common/config/CommonAutoConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.ubb.modernstore.common.config;
-
-import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.context.annotation.Import;
-
-@AutoConfiguration
-@Import(WebConfig.class)
-public class CommonAutoConfiguration {
-}
