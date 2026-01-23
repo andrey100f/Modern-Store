5a4da934869f9583a0a1e90d631743c2fa174a9e
andrey100f <andreiforminte19@yahoo.com>
2026-01-23 11:23:11 +0200

    Added initial backend code

---
+++ backend/src/main/java/com/ubb/modernstore/ModernstoreApplication.java
@@ -0,0 +1,13 @@
+package com.ubb.modernstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModernstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModernstoreApplication.class, args);
+	}
+
+}
