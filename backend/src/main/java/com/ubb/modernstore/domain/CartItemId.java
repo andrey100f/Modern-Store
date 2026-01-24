6e4a8d701207a727155481f6baa2b08f2024b69d
andrey100f <andreiforminte19@yahoo.com>
2026-01-24 15:17:20 +0200

    Add CartItem and User models with embedded cart functionality

---
+++ backend/src/main/java/com/ubb/modernstore/domain/CartItemId.java
@@ -0,0 +1,10 @@
+package com.ubb.modernstore.domain;
+
+public class CartItemId {
+
+    public static final String PRODUCT = "product";
+    public static final String QUANTITY = "quantity";
+
+    private CartItemId() {}
+
+}
